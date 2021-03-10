c3dbe4dd79b3c34d23056f6b82f7bd625bc1acf1
Grant <grant.solomons@mezzanineware.com>
2021-03-10 15:55:08 +0200

    removed text file, using json file

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
@@ -39,16 +40,21 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password
+    }}
 
     if len(website) == 0 or len(password) == 0 or email == 0:
         messagebox.showinfo(title='Oooops', message='Please make sure you have not left any field empty')
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f'The are the details entered: \nEmail: {email}'
-                                                              f'\nPassword: {password} \nIs it ok to save?')
-    if is_ok is True:
-        with open('data.text', 'a') as data_file:
-            data_file.write(f'{website} | {email} | {password}\n')
+        with open('data.json', 'r') as data_file:
+            data = json.load(data_file)
+            data.update(new_data)
+
+        with open('data.json', 'w') as data_file:
+            json.dump(data, data_file, indent=4)
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
